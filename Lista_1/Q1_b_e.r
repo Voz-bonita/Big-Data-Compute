d8067dde422c229d1d5802ecf9b34e8888b0d622
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-07-19 20:24:59 -0300

    Item d)

--- Lista_1/Q1_b_e.r
+++ Lista_1/Q1_b_e.r
@@ -4,7 +4,8 @@ pacman::p_load("vroom", "knitr", "dplyr")
 # Questão 1
 ## Item b)
 dados_path <- "./Lista_1/dados/"
-primeiro <- vroom(paste0(dados_path, "AC-Parte_1.csv"),
+arquivo1 <- paste0(dados_path, "AC-Parte_1.csv")
+primeiro <- vroom(arquivo1,
     delim = ";",
     num_threads = 4,
     locale = locale(
@@ -29,3 +30,20 @@ length(arquivos)
 bytes <- file.size(arquivos) %>% sum()
 bytes / 1024^2 # Megabytes
 bytes / 1024^3 # Gigabytes
+
+## Item d)
+unique(primeiro$vacina_nome)
+### ASTRAZENECA e ASTRAZENECA/FIOCRUZ
+
+astrazeneca <- vroom(
+    pipe(paste0("grep -i ASTRAZENECA ", arquivo1)),
+    delim = ";",
+    num_threads = 4,
+    locale = locale(
+        grouping_mark = ".",
+        decimal_mark = ",",
+        encoding = "UTF-8"
+    ),
+    show_col_types = FALSE,
+    col_names = names(primeiro)
+)

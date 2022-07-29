1e524adfcc13e2e39b07560b320937c18250493e
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-07-29 20:24:24 -0300

    Leitura dos dados 1a)

--- Lista_1/Q2.r
+++ Lista_1/Q2.r
@@ -1,4 +1,5 @@
-pacman::p_load("data.table", "glue")
+# \ devtools::install_github("ipeaGIT/geobr", subdir = "r-package")
+pacman::p_load("data.table", "glue", "geobr", "dplyr")
 
 
 # Questão 2
@@ -6,5 +7,14 @@ pacman::p_load("data.table", "glue")
 dados_path <- "./Lista_1/dados"
 arquivos <- glue("{dados_path}/{list.files(dados_path)}")
 
-dados <- lapply(arquivos[1], fread, sep = ";", select = 1:3L) %>%
+dados <- lapply(arquivos[1:3], fread,
+    sep = ";",
+    select = c(
+        "estabelecimento_uf",
+        "vacina_descricao_dose",
+        "estabelecimento_municipio_codigo"
+    )
+) %>%
     rbindlist()
+
+regios_saude <- read_health_region()

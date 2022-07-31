449131cac200e4f3ea42f5c228ef0149d646b28d
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-07-31 19:58:20 -0300

    Armazenamento dos códigos em json

--- Lista_1/Q2_codIBGE.py
+++ Lista_1/Q2_codIBGE.py
@@ -1,10 +1,12 @@
 import requests
+import json
+
 
 IBGE = "https://sage.saude.gov.br/paineis/regiaoSaude/lista.php?output=jsonbt&&order=asc&_=1659303484372"
 response = requests.get(IBGE)
 mapa_codigos = response.json()
 
-with open("./Lista_1/mapa_codigos.csv", "w+") as file:
-    file.writelines("estabelecimento_municipio_codigo, code_health_region\n")
-    for registro in mapa_codigos:
-        file.writelines(f'{registro["ibge"]}, {registro["co_colegiado"]}\n')
+dict_codigos = {registro["ibge"]: registro["co_colegiado"] for registro in mapa_codigos}
+
+with open("./Lista_1/mapa_codigos.json", "w") as output:
+    json.dump(dict_codigos, output)

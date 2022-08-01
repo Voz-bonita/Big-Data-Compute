a7d54018ac7f477d36ef4ac2ff6229f5ea3d4cd8
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-08-01 08:53:50 -0300

    Sintaxe de script executável do Python

--- Lista_1/Q2_codIBGE.py
+++ Lista_1/Q2_codIBGE.py
@@ -2,11 +2,18 @@ import requests
 import json
 
 
-IBGE = "https://sage.saude.gov.br/paineis/regiaoSaude/lista.php?output=jsonbt&&order=asc&_=1659303484372"
-response = requests.get(IBGE)
-mapa_codigos = response.json()
+def main():
+    IBGE = "https://sage.saude.gov.br/paineis/regiaoSaude/lista.php?output=jsonbt&&order=asc&_=1659303484372"
+    response = requests.get(IBGE)
+    mapa_codigos = response.json()
 
-dict_codigos = {registro["ibge"]: registro["co_colegiado"] for registro in mapa_codigos}
+    dict_codigos = {
+        registro["ibge"]: registro["co_colegiado"] for registro in mapa_codigos
+    }
 
-with open("./Lista_1/mapa_codigos.json", "w") as output:
-    json.dump(dict_codigos, output)
+    with open("./Lista_1/mapa_codigos.json", "w") as output:
+        json.dump(dict_codigos, output)
+
+
+if __name__ == "__main__":
+    main()

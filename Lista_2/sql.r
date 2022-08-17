58e395fdcc02bd6160a060338aff362c32fed851
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-08-17 17:42:14 -0300

    Faixas com SQL correto

--- Lista_2/sql.r
+++ Lista_2/sql.r
@@ -1,15 +1,14 @@
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load("DBI", "RSQLite")
+pacman::p_load("DBI", "RSQLite", "glue")
 source("./Lista_2/banco.r")
 
 
 # Questão 2
 ## Itens a) e b)
 sql_conn <- dbConnect(RSQLite::SQLite(), "./Lista_2/lista2.sqlite")
+
 dbWriteTable(sql_conn, "vax", vax_all)
-
 dbWriteTable(sql_conn, "codigos", codigos)
-
 
 seg_dose_query <- "SELECT * FROM vax
                    WHERE (vacina_descricao_dose='2ª Dose'
@@ -27,12 +26,13 @@ qnt_vax_query <- glue("SELECT regiao_saude, COUNT(*) AS N
                        FROM ({fast_join})
                        GROUP BY regiao_saude")
 
-faixa_query <- glue("SELECT regiao_saude, N,
-                        CASE WHEN N > (SELECT MEDIAN(N) FROM ({qnt_vax_query}))
+faixa_query <- glue("WITH qntVax AS ({qnt_vax_query})
+                    SELECT regiao_saude, N,
+                        CASE WHEN N > (SELECT MEDIAN(N) FROM qntVax)
                             THEN 'Alto'
                             ELSE 'Baixo'
                             END AS Faixa
-                    FROM qnt_vax")
+                    FROM qntVax")
 
 bot5_query <- glue("WITH tabFaixa AS ({faixa_query})
                     SELECT *
@@ -42,6 +42,6 @@ bot5_query <- glue("WITH tabFaixa AS ({faixa_query})
                     FROM (SELECT * FROM tabFaixa ORDER BY N DESC LIMIT 5)
                     ORDER BY N")
 
-ans <- dbGetQuery(sql_conn, bot5_query)
 
+ans <- dbGetQuery(sql_conn, faixa_query)
 dbDisconnect(sql_conn)

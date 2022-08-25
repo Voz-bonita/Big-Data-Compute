6113e9242554185c636b017a440d73b0dd3b17c5
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-08-25 20:09:27 -0300

    Web Scrapping dos arquivos do SINASC

---
+++ Lista_3/baixar.py
@@ -0,0 +1,64 @@
+from lxml import html
+import requests
+import zipfile, io
+import os
+
+
+def download(link, path):
+    r = requests.get(link)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    if not os.path.exists(path):
+        os.mkdir(path)
+    z.extractall(path)
+
+
+estados = [
+    "AC",
+    "AL",
+    "AM",
+    "AP",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MG",
+    "MS",
+    "MT",
+    "PA",
+    "PB",
+    "PE",
+    "PI",
+    "PR",
+    "RJ",
+    "RN",
+    "RO",
+    "RR",
+    "RS",
+    "SC",
+    "SE",
+    "SP",
+    "TO",
+]
+anos = range(1996, 2021)
+
+src = "https://datasus.saude.gov.br/wp-content/download.php"
+db = "ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/Dados/DNRES"
+payload = {}
+
+k = 0
+for estado in estados:
+    print(estado)
+    for ano in anos:
+        print(anos)
+        file = f"DN{estado}{ano}.dbc"
+        payload = {
+            "dados[0][arquivo]": file,
+            "dados[0][link]": f"{db}/{file}",
+        }
+        response = requests.post(src, payload)
+
+        zip_dbc = response.json()[0][0].replace("//", "/").replace("/", "//")
+
+        download(zip_dbc, path="./Lista_3/datasus")

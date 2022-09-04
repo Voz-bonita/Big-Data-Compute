6e02578a27891b785a0f059a1f5a913474bb2774
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-09-04 09:16:53 -0300

    Escolha das colunas com base em NAs

--- Lista_3/questao2.r
+++ Lista_3/questao2.r
@@ -1,5 +1,8 @@
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load("arrow", "dplyr", "stringr", "sparklyr", "lubridate", "ggplot2")
+pacman::p_load(
+    "arrow", "dplyr", "stringr", "sparklyr",
+    "lubridate", "ggplot2", "glue"
+)
 source("./Lista_1/funcoes_aux.r")
 
 sc <- spark_connect(master = "local", version = "2.4.3")
@@ -13,7 +16,7 @@ sinasc <- spark_read_parquet(
     select(-c(
         contador, LOCNASC, CODMUNNASC,
         CODMUNRES, APGAR1, APGAR5,
-        RACACOR, CODANOMAL, PESO,
+        RACACOR, CODANOMAL,
     )) %>%
     mutate(DTNASC = from_unixtime(unix_timestamp(DTNASC, "ddMMyyyy"), "u")) %>%
     mutate(CODOCUPMAE = as.numeric(CODOCUPMAE)) %>%
@@ -25,6 +28,7 @@ sinasc <- spark_read_parquet(
     mutate(GRAVIDEZ = as.numeric(GRAVIDEZ)) %>%
     mutate(IDADEMAE = as.numeric(IDADEMAE)) %>%
     mutate(PARTO = as.numeric(PARTO)) %>%
+    mutate(PESO = as.numeric(PESO)) %>%
     mutate(QTDFILMORT = as.numeric(QTDFILMORT)) %>%
     mutate(QTDFILVIVO = as.numeric(QTDFILVIVO)) %>%
     mutate(SEXO = as.numeric(SEXO))
@@ -45,12 +49,19 @@ sinasc %>%
         "latex"
     )
 
-sinasc %>%
-    summarise_all(~ sum(as.integer(is.na(.))))
 
-qnt_vars <- c("IDADEMAE", "QTDFILVIVO")
+linhas <- sdf_dim(sinasc)[1]
+faltante <- sinasc %>%
+    summarise_all(~ sum(as.integer(is.na(.)) / linhas)) %>%
+    collect()
 
-sinasc %>%
+sinasc <- sinasc %>%
+    select(-colnames(faltante[which(faltante > 0.15)]))
+
+
+qnt_vars <- c("IDADEMAE", "QTDFILVIVO", "PESO")
+
+sinasc <- sinasc %>%
     ft_imputer(
         input_cols = qnt_vars,
         output_cols = qnt_vars,
@@ -63,10 +74,15 @@ sinasc %>%
     ft_standard_scaler(
         input_col = "qnt_features",
         output_col = "standard_qnt"
-    )
-
-sinasc %>%
+    ) %>%
     filter(!is.na(DTNASC)) %>%
+    mutate(CONSULTAS = if_else(is.na(CONSULTAS), 9, CONSULTAS)) %>%
+    mutate(GRAVIDEZ = if_else(is.na(GRAVIDEZ), 9, GRAVIDEZ)) %>%
+    mutate(GESTACAO = if_else(is.na(GESTACAO), 9, GESTACAO)) %>%
+    mutate(ESCMAE = if_else(is.na(ESCMAE), 9, ESCMAE))
+
+
+sinasc <- sinasc %>%
     ft_one_hot_encoder(
         input_cols = "DTNASC",
         output_cols = "DTNASC_dummy"
@@ -78,17 +94,23 @@ sinasc %>%
     )
 
 
+#--------- Modelo de Regressão ---------#
 particoes <- sinasc %>%
     mutate(PARTO = if_else(PARTO != 2, 1, 0, 0)) %>%
+    filter(!is.na(CONSULTAS)) %>%
     sdf_random_split(training = 0.8, test = 0.2, seed = 2022)
 
 treino <- particoes$training
 teste <- particoes$test
 
-formula <- ("PARTO ~ IDADEMAE")
+label_out <- "PARTO"
+features <- paste(
+    colnames(select(sinasc, -c(qnt_vars, qnt_features))),
+    collapse = " + "
+)
+formula <- (glue("{label_out} ~ features"))
 lr_model <- ml_logistic_regression(treino, formula)
 
-# Prevendo usando o spark - Predict()
 validation_summary <- ml_evaluate(lr_model, teste)
 
 roc <- validation_summary$roc() %>%

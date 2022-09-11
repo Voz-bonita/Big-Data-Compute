98a48cfcccd4bedd90a56828525b398fa03035b3
Jose Vitor <jose.vitor.sem.c@gmail.com>
2022-09-11 18:51:56 -0300

    Item a) completo

---
+++ Lista_4/main.r
@@ -0,0 +1,30 @@
+if (!require(pacman)) install.packages("pacman")
+pacman::p_load("readxl", "dplyr", "stringr", "purrr")
+set.seed(2022)
+
+
+serie <- read_xlsx(
+    "./Lista_4/Estatisticas_times.xlsx",
+    col_types = c("text", "numeric", "numeric", "numeric")
+) %>%
+    mutate(GF = Gols_feitos / Jogos) %>%
+    mutate(GS = Gols_sofridos / Jogos) %>%
+    mutate(Pais = str_sub(Pais, 2L, -1L))
+
+#--- Lista de parâmetros ---#
+gf <- pull(serie, GF, name = Pais)
+gs <- pull(serie, GS, name = Pais)
+lambda_ij <- map(gf, ~ (.x + gs) / 2)
+#--- #
+
+# Item a)
+#--- Probabilidade exata ~~ 0.00434 ~~ 0.4%
+dpois(5, lambda_ij[["Brazil"]]["Serbia"]) *
+    dpois(0, lambda_ij[["Serbia"]]["Brazil"])
+
+
+n <- 10^6
+br <- rpois(n, lambda_ij[["Brazil"]]["Serbia"])
+serv <- rpois(n, lambda_ij[["Serbia"]]["Brazil"])
+sum(br == 5 && serv == 0) / n
+#--- Probabilidade estimada ~~ 0%
